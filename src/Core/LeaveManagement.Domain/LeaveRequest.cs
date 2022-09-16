e9e1aeb0a08b0547a86df6db736aff43145be84b
Pylyp <pylyp.vynnykov@justeattakeaway.com>
2022-09-16 16:06:24 +0200

    Add project files.

---
+++ src/Core/LeaveManagement.Domain/LeaveRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeaveManagement.Domain
+{
+    public class LeaveRequest
+    {
+        public int Id { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int DefaultDays { get; set; }
+
+    }
+}
---
+++ src/Core/LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeaveManagement.Domain
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int DefaultDays { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
